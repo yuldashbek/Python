d8b55a26c8c912623ee74a8e0ba682a2d276ddbf
yuldashbek <yuldashbec@gmail.com>
2025-06-07 15:11:26 +0500

    Create hm.py

---
+++ lesson-1/homework/hm.py
@@ -0,0 +1,24 @@
+a= int(input('enter height:'))
+b= int(input('enter width:'))
+perimeter= 2*(a+b)
+area= a*b
+perimeter
+area
+
+diameter= int(input('enter diameter:'))
+lengt = 3.14*diameter
+lengt
+
+a= int(input('enter number:'))
+b= int(input('enter number:'))
+mean= (a+b)/2
+mean
+
+a = int(input('enter number:'))
+b = int(input('enter number:'))
+Sum= a + b 
+Square_a= a**2
+Square_b= b**2
+Sum
+Square_a
+Square_b
