176962dd096b83bf02860ced26c426ed8990214e
yuldashbek <yuldashbec@gmail.com>
2025-06-10 10:07:14 +0500

    hm.py

--- lesson-1/homework/hm.py
+++ lesson-1/homework/hm.py
@@ -2,23 +2,23 @@ a= int(input('enter height:'))
 b= int(input('enter width:'))
 perimeter= 2*(a+b)
 area= a*b
-perimeter
-area
+print(perimeter)
+print(area)
 
 diameter= int(input('enter diameter:'))
 lengt = 3.14*diameter
-lengt
+print(lengt)
 
 a= int(input('enter number:'))
 b= int(input('enter number:'))
 mean= (a+b)/2
-mean
+print(mean)
 
 a = int(input('enter number:'))
 b = int(input('enter number:'))
 Sum= a + b 
 Square_a= a**2
 Square_b= b**2
-Sum
-Square_a
-Square_b
+print(Sum)
+print(Square_a)
+print(Square_b)
